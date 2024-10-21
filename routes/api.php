841271fbd61dd27f2014869bb9f24d76bfe5564c
Axentioi ALexandru <Alexandru.Axentioi@sag-digital.de>
2024-10-21 20:15:13 +0300

    latest

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\GamesController;
+use App\Models\Games;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/games', [GamesController::class, 'index']);
+Route::post('/games/{game}/join', [GamesController::class, 'join']);
+Route::post('/games/{game}/leave', [GamesController::class, 'leave']);
